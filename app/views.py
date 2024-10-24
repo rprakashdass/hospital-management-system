ccdac7db81ddbd9b427673346b11761f56701406
Prakash Dass R <123339797+rprakashdass@users.noreply.github.com>
2024-10-24 17:54:51 +0530

    Add files via upload

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+<<<<<<< HEAD
+=======
 @login_required
+>>>>>>> 25c35da1111a0e159012650888808c7017ef0938
 def home(request):
     return render(request, 'home.html')
