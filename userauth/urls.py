1d2b5642e56419a47fc909e43f5dbf1234ac5c92
rprakashdass <rprakashdass@gmail.com>
2024-12-06 17:13:11 +0530

    fix routing bugs

--- userauth/urls.py
+++ userauth/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name = 'userauth'
 urlpatterns = [
-    path('register/<str:role>/', views.register_view, name='register'),
+    path('register/<str:role>/', views.register_view, name='register_user'),
     path('login/<str:role>/', views.login_user, name='login_user'),
     path('logout/', views.logout_user, name='logout'),
     path('', views.home, name='home'),
